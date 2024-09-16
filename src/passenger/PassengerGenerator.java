fbb3a4c0374b3933a1f71b6c9a69f98eb20ca9f8
alex <aleksandr.sajenko@gmail.com>
2024-09-16 16:52:11 +0300

    Improve passenger generation
    Added generateResidentialPassenger(), and a placeholder for
    generateOfficeWorkerPassenger(). There will be more separation between
    residential and office building logic, though residential will be
    developed first.

--- src/passenger/PassengerGenerator.java
+++ src/passenger/PassengerGenerator.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class PassengerGenerator implements Runnable {
 
     private boolean shouldRun = true;
-    private static final long GENERATION_INTERVAL = 100;
+    private static final long GENERATION_INTERVAL = 1000;
 
     private final Building building;
     private final CustomLogger logger;
@@ -26,34 +26,48 @@ public class PassengerGenerator implements Runnable {
     public void run() {
         logger.logMain("Started");
         while (shouldRun) {
-            generatePassenger();
-            try {
-                Thread.sleep(GENERATION_INTERVAL);
-            } catch (InterruptedException e) {
-                logger.logError(e);
-            }
+            generateResidentialPassenger();
+            executeGenerationInterval();
         }
         logger.logMain("Stopped");
     }
 
-    private int getRandomDestinationFloorNumber(Integer currentFloorNumber) {
+    private int getRandomFloorNumberExcept(Integer floorNumberToExclude) {
         ArrayList<Integer> options = building.getFloors().stream()
-                .filter(floor -> !currentFloorNumber.equals(floor.floorNumber))
+                .filter(floor -> !floorNumberToExclude.equals(floor.floorNumber))
                 .map(floor -> floor.floorNumber)
                 .collect(Collectors.toCollection(ArrayList::new));
         return options.get(random.nextInt(options.size()));
     }
 
-    private void generatePassenger() {
-        int floorNumber = random.nextInt(building.getFloors().size()) + 1;
-        int destinationFloorNumber = getRandomDestinationFloorNumber(floorNumber);
-        Floor floor = building.getFloorByNumber(floorNumber);
+    // Residential passengers only move between 1st floor and the floor they reside.
+    // There's 50% chance they start at 1st floor.
+    private void generateResidentialPassenger() {
+        int startingFloorNumber;
+        int destinationFloorNumber;
+
+        boolean coinFlip = random.nextBoolean();
+        if (coinFlip) {
+            startingFloorNumber = 1;
+            destinationFloorNumber = getRandomFloorNumberExcept(startingFloorNumber);
+        } else {
+            startingFloorNumber = getRandomFloorNumberExcept(1);
+            destinationFloorNumber = 1;
+        }
+
+        Floor floor = building.getFloorByNumber(startingFloorNumber);
         Passenger passenger = new Passenger(destinationFloorNumber);
         synchronized (building.getFloors()) {
             floor.addWaitingPassenger(passenger);
             floor.getButton().press();
         }
-        // logger.logPassengers("Generated passenger at floor " + floorNumber + ", dest: " + destinationFloorNumber);
+        logger.logPassengers("Generated passenger at floor " + startingFloorNumber + ", dest: " + destinationFloorNumber);
+    }
+
+    // Office workers can go between random floors. There's 2 in 3 chance they start or finish at 1st floor, and 1 in 3
+    // chance they start and finish on random floors.
+    private void generateOfficeWorkerPassenger() {
+        // TODO: Implement this method
     }
 
     private void executeGenerationInterval() {
