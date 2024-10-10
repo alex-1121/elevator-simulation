e04eaf72971cc08c6729c9427a58e2d12aa20623
alex <aleksandr.sajenko@gmail.com>
2024-10-10 20:09:49 +0300

    Replace LinkedList with ArrayList for floors list
    The current implementation does not utilize any properties specific to
    LinkedList. Switching to ArrayList for simplicity.

--- src/building/Building.java
+++ src/building/Building.java
@@ -1,14 +1,13 @@
 package building;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 
 public class Building {
-    private final Collection<Floor> floors = Collections.synchronizedCollection(new LinkedList<>());
+    private final Collection<Floor> floors = Collections.synchronizedList(new ArrayList<>());
 
     public Building(int numberOfFloors) {
-
         for (int i = 0; i < numberOfFloors; i++) {
             floors.add(new Floor(i + 1));
         }
